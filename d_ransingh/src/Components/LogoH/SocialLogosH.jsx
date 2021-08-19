006ab4b970d138158a614115d94112c387f607ac
Ransingh88 <rajaransingh@gmail.com>
2021-08-19 23:15:33 +0530

    twitter link added

--- d_ransingh/src/Components/LogoH/SocialLogosH.jsx
+++ d_ransingh/src/Components/LogoH/SocialLogosH.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { FaGithub,FaMediumM,FaYoutube,FaInstagram,FaLinkedinIn,FaBehance } from 'react-icons/fa';
+import { FaGithub,FaMediumM,FaYoutube,FaInstagram,FaLinkedinIn,FaBehance,FaTwitter } from 'react-icons/fa';
 import { SiHashnode, SiGmail } from 'react-icons/si';
 
 
@@ -31,6 +31,7 @@ const SocialLogosH = () => {
             <a href="https://medium.com/me/stories/public" target="_blank" rel="noopener noreferrer"><span><FaMediumM/></span></a>
             <a href="https://www.youtube.com/channel/UC9bqAMPdiYbsX5ZcpCBrNnw" target="_blank" rel="noopener noreferrer"><span><FaYoutube/></span></a>
             <a href="https://www.instagram.com/d.ransingh88/" target="_blank" rel="noopener noreferrer"><span><FaInstagram/></span></a>
+            <a href="https://twitter.com/Ransingh88" target="_blank" rel="noopener noreferrer"><span><FaTwitter/></span></a>
             <a href="https://www.linkedin.com/in/debasish-ransingh-612437154/" target="_blank" rel="noopener noreferrer"><span><FaLinkedinIn/></span></a>
             <a href="https://www.behance.net/debasisransing" target="_blank" rel="noopener noreferrer"><span><FaBehance/></span></a>
             <a href="https://dransingh88.hashnode.dev/" target="_blank" rel="noopener noreferrer"><span><SiHashnode/></span></a>
