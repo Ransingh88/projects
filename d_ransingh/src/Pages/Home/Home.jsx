1dda651ad4038f4dbb8d1db91a7708fe9ef37bb7
Ransingh88 <rajaransingh@gmail.com>
2021-08-30 22:53:29 +0530

    home page updated

--- d_ransingh/src/Pages/Home/Home.jsx
+++ d_ransingh/src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Home.module.css'
 import styled from 'styled-components'
 import { SocialLogosH } from '../../Components/LogoH/SocialLogosH'
@@ -7,28 +7,64 @@ import { SiMinutemailer } from 'react-icons/si';
 import { Card } from '../../Components/Card/Card'
 
 const Name = styled.h1`
-color: whitesmoke;
-font-size: 2.5rem;
+color: #252525;
+font-size: 5rem;
 text-align: center;
 letter-spacing: 0.1rem;
+font-weight: 900;
 
 `
 const Work = styled.p`
-color: whitesmoke;
+color: #252525;
 font-size: 1.2rem;
 text-align: center;
 letter-spacing: 0.1rem;
 text-transform:uppercase;
 
 `
+const Warning = styled.div`
+position: fixed;
+top: 5rem;
+right: 3rem;
+border-radius: 0.2rem;
+background-color: #ffffa1;
+border: 2px solid #ffee00;
+padding: 1rem 2rem;
+color: #252525;
+box-shadow: 0 0 10px #ddd;
+visibility: ${props=>props.vs ? "hidden" : "visible"};
+button{
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    height: 20px;
+    width: 20px;
+    background-color: #ffee00;
+    border: 1px solid #ffee00;
+    color: #252525;
+    font-weight: bold;
+    border-radius: 50%;
+    border: 0;
+    outline: 0;
+}
+button:hover{
+    cursor: pointer;
+}
+
+`
 
 const Home = () => {
+    const [vs,setVs] = useState(false)
+
+    const handleWarning = () => {
+        setVs(true)
+    }
     return (
         <div className={styles.Container}>
         <div className={styles.mainContainer}>
-            
+            <Warning vs={vs}><p>! The  website is under construction</p><button onClick={handleWarning}>X</button></Warning>
             <img src="./images/picSquare.jpg" alt="ProfileImage" />
-            <Name>DEBASISH RANSINGH</Name>
+            <Name>Hi I'm DEBASISH RANSINGH</Name>
             <Work>Full Stack Web Developer</Work>
             <SocialLogosH />
             <p className={styles.summery}>Detailed-oriented, responsible, and committed developer, with a get-it-done, on-time, and high-quality product spirit, and more than a year of experience defining requirements, designing, implementing, testing, and delivering complex back-end and web applications using verity programming and technologies.</p>
