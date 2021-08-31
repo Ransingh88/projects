6186e946159902051159e9a5d293d32eaabcbd0b
Ransingh88 <rajaransingh@gmail.com>
2021-08-31 23:32:27 +0530

    card code updated

--- d_ransingh/src/Components/Card/Card.jsx
+++ d_ransingh/src/Components/Card/Card.jsx
@@ -13,8 +13,11 @@ const Card = ({img,title,desc}) => {
                     <div className={styles.content}>
                         <img src="./images/portfolio.png" alt="" />
                         <div className={styles.bodyContents}>
-                        <h2>Card</h2>
+                            <h2>Card</h2>
+                        <div className={styles.projectDesc}>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, iste?</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, iste?</p>
+                        </div>
                         <div className={styles.techStack}>
                                 <img src="./icons/react-2.svg" alt="" height="25px"/>
                                 <img src="./icons/logo-javascript.svg" alt="" height="25px"/>
@@ -25,6 +28,7 @@ const Card = ({img,title,desc}) => {
                                 <img src="./icons/github-icon-1.svg" alt="" height="25px"/>
                                 <img src="./icons/bootstrap-5-1.svg" alt="" height="25px"/>
                                 <img src="./icons/firebase-1.svg" alt="" height="25px"/>
+                                <img src="./icons/netlify.svg" alt="" height="25px"/>
                             </div>
                             
                             <div className={styles.links}>
