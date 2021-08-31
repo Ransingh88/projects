a4c0751f733a3abd1e8d08bc19d7a760722f397c
Ransingh88 <rajaransingh@gmail.com>
2021-08-31 23:03:02 +0530

    new card design updated

--- d_ransingh/src/Components/Card/Card.jsx
+++ d_ransingh/src/Components/Card/Card.jsx
@@ -1,62 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
+import styles from './Card.module.css'
+import { FiExternalLink,FiGithub } from 'react-icons/fi';
 
-const Cards = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-width: 250px;
-border-radius: 0.2rem;
-border: 1px solid #eee;
-margin: 1rem;
-img{
-    width: 100%;
-    border-radius: 0.2rem;
-}
-h3{
-    text-align: center;
-}
-:hover{
-    
-box-shadow: 0 0 10px #aaa;
-    transform: scale(1.01);
-    transition: ease 0.2s;
-    cursor: pointer;
-}
-.Content{
-    padding: 0.5rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-`
-const Button = styled.div`
-border-radius: 0.4rem;
-background: royalblue;
-padding: 0.4rem 0.8rem;
-margin: 0.5rem;
-display: inline-block;
-color: whitesmoke;
-font-weight: 500;
 
-:hover{
-   background-color: #2353e4;
-}
-
-`
 
 const Card = ({img,title,desc}) => {
     return (
-        <Cards>
-            <img src={img} alt="portfolio Project" />
-            <div className="Content">
-            <h3>{title}</h3>
-            <p>{ desc }</p>
-            <Button>VISIT SITE</Button>
+        <div className={styles.Container}>
+            <div className={styles.card}>
+                <div className={styles.box}>
+                    <div className={styles.content}>
+                        <img src="./images/portfolio.png" alt="" />
+                        <div className={styles.bodyContents}>
+                        <h2>Card</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, iste?</p>
+                        <div className={styles.techStack}>
+                                <img src="./icons/react-2.svg" alt="" height="25px"/>
+                                <img src="./icons/logo-javascript.svg" alt="" height="25px"/>
+                                <img src="./icons/html-1.svg" alt="" height="25px"/>
+                                <img src="./icons/css-3.svg" alt="" height="25px"/>
+                                <img src="./icons/redux.svg" alt="" height="25px"/>
+                                <img src="./icons/nodejs-icon.svg" alt="" height="25px"/>
+                                <img src="./icons/github-icon-1.svg" alt="" height="25px"/>
+                                <img src="./icons/bootstrap-5-1.svg" alt="" height="25px"/>
+                                <img src="./icons/firebase-1.svg" alt="" height="25px"/>
+                            </div>
+                            
+                            <div className={styles.links}>
+                                    <p className={styles.demo}><FiExternalLink/> &nbsp; Demo</p>
+                                    <p className={styles.code}><FiGithub/> &nbsp; Code</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </Cards>
+        </div>
     )
 }
 
