7602c51f24899150a2629b528036027f65124ae1
Ransingh88 <rajaransingh@gmail.com>
2021-09-01 22:52:56 +0530

    code updated

--- d_ransingh/src/App.jsx
+++ d_ransingh/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Navbar } from './Components/Navbar/Navbar';
+import { Technologies } from './Components/Skills/Technologies';
 import {Home} from './Pages/Home/Home'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Home />
+      {/* <Technologies/> */}
     </div>
   );
 }
