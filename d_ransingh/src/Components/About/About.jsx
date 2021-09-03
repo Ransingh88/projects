7db4d14b6fd4fa1822b45593ef804c6f861c5766
Ransingh88 <rajaransingh@gmail.com>
2021-09-03 23:59:03 +0530

    link added

--- d_ransingh/src/Components/About/About.jsx
+++ d_ransingh/src/Components/About/About.jsx
@@ -14,9 +14,9 @@ const About = () => {
                     <br />
                     <p>Detailed-oriented, responsible, and committed developer, with a get-it-done, on-time, and high-quality product spirit, and more than a year of experience defining requirements, designing, implementing, testing, and delivering complex back-end and web applications using verity programming and technologies.</p>
                         <div className={styles.links}>
-                            <img src="./icons/github-icon-1.svg" alt="" />
-                            <img src="./icons/linkedin-icon-2.svg" alt="" />
-                            <img src="./icons/monogram-medium.svg" alt="" />
+                            <a href="https://github.com/Ransingh88" target="_blank" rel="noopener noreferrer"><img src="./icons/github-icon-1.svg" alt="" /></a>
+                            <a href="https://www.linkedin.com/in/debasish-ransingh-612437154/" target="_blank" rel="noopener noreferrer"><img src="./icons/linkedin-icon-2.svg" alt="" /></a>
+                            <a href="https://medium.com/@d_ransingh88" target="_blank" rel="noopener noreferrer"><img src="./icons/monogram-medium.svg" alt="" /></a>
                     </div>
                     </div>
                     </div>
