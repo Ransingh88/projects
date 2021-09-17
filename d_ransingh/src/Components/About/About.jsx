db37b361c6caa5063d0c03a310d1746a96128156
Ransingh88 <rajaransingh@gmail.com>
2021-09-17 20:55:15 +0530

    about spelling corected

--- d_ransingh/src/Components/About/About.jsx
+++ d_ransingh/src/Components/About/About.jsx
@@ -10,9 +10,9 @@ const About = () => {
                 <div className={styles.aboutContent}>
                 <img src="./images/aboutme.svg" alt="aboutme" />
                 <div className={styles.content}>
-                    <p>Detailed-oriented, responsible, and committed developer, with a get-it-done, on-time, and high-quality product spirit, and more than a year of experience defining requirements, designing, implementing, testing, and delivering complex back-end and web applications using verity programming and technologies.</p>
+                    <p>Detailed-oriented, responsible, and committed developer, with a get-it-done, on-time, and high-quality product spirit, and more than a year of experience defining requirements, designing, implementing, testing, and delivering complex back-end and web applications using a variety of programming and technologies.</p>
                     <br />
-                    <p>Love to travel and explore.It refresh my mind, body, anxiety, stress and keeps me active all the time.While exproring keeping all the moment as memory in camera, and this is how you became photographer also. And I enjoying journey than destination</p>
+                    <p>Love to travel and explore. It refreshes my mind, body, anxiety, stress and keeps me active all the time. While exploring keeping all the moments as memory in the camera, and this is how you became a photographer also. And I enjoying the journey than the destination</p>
                         <div className={styles.links}>
                             <a href="https://github.com/Ransingh88" target="_blank" rel="noopener noreferrer"><img src="./icons/github-icon-1.svg" alt="" /></a>
                             <a href="https://www.linkedin.com/in/debasish-ransingh-612437154/" target="_blank" rel="noopener noreferrer"><img src="./icons/linkedin-icon-2.svg" alt="" /></a>
