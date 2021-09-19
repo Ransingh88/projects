ab677ce1b889ca2e8d578eebe36e11267a9373e4
Ransingh88 <rajaransingh@gmail.com>
2021-09-19 13:58:07 +0530

    code updated

--- d_ransingh/src/Components/Hero/Hero.jsx
+++ d_ransingh/src/Components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className={styles.hero}>
             <div className={styles.left}>
                 <h1 className={styles.name}>Hi I'm <br /> DEBASISH RANSINGH</h1>
-                <p data-text ="FULL STACK WEB DEVELOPER 💻" className={styles.designation}>FULL STACK WEB DEVELOPER 💻</p>
+                <p  className={styles.designation}>FULL STACK WEB DEVELOPER 💻</p>
                 <p className={styles.shortDesc}>An enthusiastic Full Stack Web Developer with a strong set of technical as well as non-technical skills and a dedication towards creating useful and interactive web applications.</p>
                 <a href="https://drive.google.com/file/d/1YV8OpaMyZmNYqGPBEqehl2n_w4Psq-Rl/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                 <p className={styles.resumeBtn}>Resume</p>
