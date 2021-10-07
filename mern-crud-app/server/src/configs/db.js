77330850cc6170e0affb1a2662c1d2865a2d2cbd
Ransingh88 <rajaransingh@gmail.com>
2021-10-07 15:23:13 +0530

    Mongo db URI  removed updated

--- mern-crud-app/server/src/configs/db.js
+++ mern-crud-app/server/src/configs/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 
-// const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb://localhost:27017/test';
-const CONNECTION_URL = 'mongodb+srv://debasish082:debasish082@cluster0.xahh7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb://localhost:27017/test';
 
 const connect = () => {
     return mongoose.connect(CONNECTION_URL)
