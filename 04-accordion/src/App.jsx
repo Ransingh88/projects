c85f5946ea004b796cd7a62bd9f8523aecc797c1
Ransingh88 <rajaransingh@gmail.com>
2021-11-06 01:25:36 +0530

    main jsx data mapped and project done

--- 04-accordion/src/App.jsx
+++ 04-accordion/src/App.jsx
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import AccordionCard from './AccordionCard';
+import data from './data'
 
 function App() {
+  
   return (
-    <div className="App">
-      Accordion
+    <div className="main">
+      <div className="main__heading">
+
+      <h1>Accordion</h1>
+      </div>
+      <div className="main__content">
+        <div className="content__head"><h1>Confused? <br /> Read Question & Answer</h1></div>
+        <div className="content__card">
+          {
+            data.map((ele) => {
+              return <AccordionCard key={ele.id} {...ele}/>
+            })
+          }
+      
+        </div>
+      </div>
     </div>
   );
 }
