ddf4e4d3b31ad6a3c092a82194b7fc86937dfc7e
Debasish_R <rajaransingh@gmail.com>
2022-04-26 12:31:26 +0530

    backend code updated installed cors

--- React-Projects/minion_studio/server/src/server.js
+++ React-Projects/minion_studio/server/src/server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const connect = require('./config/db')
+const cors = require('cors')
+
+
 const userController = require('./routes/user.route')
 const resourcesController = require('./routes/resources.route')
 
 
 require('dotenv').config()
 const app = express()
-
-
-
 const PORT = process.env.PORT || 5000
+
+
+app.use(cors())
 app.use(express.json())
 app.use('/api/user',userController)
 app.use('/api/resource',resourcesController)
