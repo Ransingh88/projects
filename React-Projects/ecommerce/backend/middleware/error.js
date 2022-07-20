345c197d3407bcf96f5bc73f9ca4044bce51fcc9
Debasish_R <rajaransingh@gmail.com>
2022-07-20 14:01:38 +0530

    jwt, duplicate email error handled

--- React-Projects/ecommerce/backend/middleware/error.js
+++ React-Projects/ecommerce/backend/middleware/error.js
@@ -11,6 +11,24 @@ module.exports = (err,req,res,next) =>{
         err = new ErrorHandler(message,400)
     }
 
+    // mongoose duplicat key error
+    if(err.code == 11000){
+        const message =`Duplicate ${Object.keys(err.keyValue)} entered. '${err.keyValue.email}' already exist`
+        err = new ErrorHandler(message,400)
+    }
+
+    // JWT invalid error
+    if(err.name === "JsonWebTokenError"){
+        const message = `Json Web Token is invalid, try again`
+        err = new ErrorHandler(message,400)
+    }
+
+     // JWT expire error
+     if(err.name === "TokenExpiredError"){
+        const message = `Json Web Token is expired, try again`
+        err = new ErrorHandler(message,400)
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message
