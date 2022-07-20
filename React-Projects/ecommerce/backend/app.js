e7f27d1f5c2d3ec4ca3950730e3f420570f3ac08
Debasish_R <rajaransingh@gmail.com>
2022-07-20 13:16:55 +0530

    route name changes and user route added

--- React-Projects/ecommerce/backend/app.js
+++ React-Projects/ecommerce/backend/app.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
 
 
 const errorMiddleware = require('./middleware/error')
 
 
 app.use(express.json())
+app.use(cookieParser())
 
 
 // Routes Imports
-const product = require('./routes/productRoute')
+const productRoute = require('./routes/productRoute')
+const userRoute = require('./routes/userRoute')
 
-app.use('/api/v1',product)
+app.use('/api/v1',productRoute)
+app.use('/api/v1',userRoute)
+
 
 // Middleware for errors
 app.use(errorMiddleware)
