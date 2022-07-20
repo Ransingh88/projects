8d4530c241649ce90fb30de8424842657020635f
Debasish_R <rajaransingh@gmail.com>
2022-07-20 13:13:43 +0530

    added authentication to routes

--- React-Projects/ecommerce/backend/routes/productRoute.js
+++ React-Projects/ecommerce/backend/routes/productRoute.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require('../controllers/productController')
+const { isAuthenticatedUser, authorizedRoles } = require('../middleware/auth')
 
 const router = express.Router()
 
 router.route('/products').get(getAllProducts)
-router.route('/products/new').post(createProduct)
-router.route('/products/:id').put(updateProduct)
-router.route('/products/:id').delete(deleteProduct)
+router.route('/products/new').post(isAuthenticatedUser,authorizedRoles("admin"),createProduct)
+router.route('/products/:id').put(isAuthenticatedUser,authorizedRoles("admin"),updateProduct)
+router.route('/products/:id').delete(isAuthenticatedUser,authorizedRoles("admin"),deleteProduct)
 router.route('/products/:id').get(getProductDetails)
 
 module.exports = router
