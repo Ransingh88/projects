b8b57d56aefbe95d305a19189a778dd4c4d71d9e
Debasish_R <rajaransingh@gmail.com>
2022-07-22 02:46:13 +0530

    order route added

--- React-Projects/ecommerce/backend/app.js
+++ React-Projects/ecommerce/backend/app.js
@@ -13,9 +13,11 @@ app.use(cookieParser())
 // Routes Imports
 const productRoute = require('./routes/productRoute')
 const userRoute = require('./routes/userRoute')
+const orderRoute = require('./routes/orderRoute')
 
 app.use('/api/v1',productRoute)
 app.use('/api/v1',userRoute)
+app.use('/api/v1',orderRoute)
 
 
 // Middleware for errors
