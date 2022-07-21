3b1a9d983e8021717bb7dd2cd0fdd849e1d3540a
Debasish_R <rajaransingh@gmail.com>
2022-07-22 00:01:42 +0530

    add review and delete review added to product

--- React-Projects/ecommerce/backend/controllers/productController.js
+++ React-Projects/ecommerce/backend/controllers/productController.js
@@ -117,3 +117,50 @@ exports.createProductReview = catchAsyncErrors(async(req,res,next)=>{
 
     })
 })
+
+// get all reviews of product
+exports.getAllReviews = catchAsyncErrors(async(req,res,next)=>{
+    const product = await Product.findById(req.query.id)
+
+    if(!product){
+        return next(new ErrorHandler("product not found",404))
+    }
+
+    res.status(200).json({
+        success:true,
+        reviews:product.reviews
+    })
+})
+
+// delete review of a product
+exports.deleteReview = catchAsyncErrors(async(req,res,next)=>{
+    const product = await Product.findById(req.query.productId)
+
+    if(!product){
+        return next(new ErrorHandler("product not found",404))
+    }
+
+    const reviews = product.reviews.filter(rev => rev._id.toString() !== req.query.id.toString())
+
+    let avg = 0
+    reviews.forEach(rev=>{
+        avg+=rev.rating
+    })
+
+    const ratings = avg/reviews.length
+    const numOfReviews = reviews.length
+
+    await Product.findByIdAndUpdate(req.query.productId,{
+        reviews,
+        ratings,
+        numOfReviews
+    },{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+    })
+
+    res.status(200).json({
+        success:true
+    })
+})
