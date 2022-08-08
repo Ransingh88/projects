68bc71490c02555578a611f08f660464f9002ae7
Debasish_R <rajaransingh@gmail.com>
2022-08-08 15:58:20 +0530

    footer

---
+++ React-Projects/ecommerce/frontend/src/components/layout/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <footer className='container-fluid mainFooterContainer'>
+      <div className='container footerContainer'>
+      <div className='row'>
+        <div className='col-3'>q</div>
+        <div className='col-3'>ee</div>
+        <div className='col-3'>ee</div>
+        <div className='col-3'>ee</div>
+        <div className='col-12'>ee</div>
+      </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
