324d7777f3f0fd8ed1be9298c3b6870c090f930e
Debasish_R <rajaransingh@gmail.com>
2022-08-08 18:54:17 +0530

    fix:home page code updated removed test codes

--- React-Projects/ecommerce/frontend/src/pages/home/Home.jsx
+++ React-Projects/ecommerce/frontend/src/pages/home/Home.jsx
@@ -4,66 +4,7 @@ import './home.css'
 const Home = () => {
   return (
     <main className='container-fluid mainHomeContainer'>
-        <div className="container homeContainer">Home Page
-        <button className='button1 button'>button 1</button>
-        <button className='button2 button'>button 2</button>
-        <button className='button3 button'>button 3</button>
-        <button className='button4 button'>button 4</button>
-        <button className='button5 button'>button 4</button>
-
-        <h1 className='heading'>Debasish <span>Ransingh</span></h1>
-
-        <div className='box1 box'>
-        <div className='box2 box'>
-        <div className='box3 box'>
-        <div className='box4 box'>
-        <div className='box5 box'>
-        erer
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-
-        <div className='boxl1 boxl'>
-        <div className='boxl2 boxl'>
-        <div className='boxl3 boxl'>
-        <div className='boxl4 boxl'>
-        <div className='boxl5 boxl'>
-        erer
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-
-        <div className='boxp1 boxp'>
-        <div className='boxp2 boxp'>
-        <div className='boxp3 boxp'>
-        <div className='boxp4 boxp'>
-        <div className='boxp5 boxp'>
-        erer
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-
-
-        <div className='boxs1 boxs'>
-        <div className='boxs2 boxs'>
-        <div className='boxs3 boxs'>
-        <div className='boxs4 boxs'>
-        <div className='boxs5 boxs'>
-        erer
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-
-
-        </div>
+        Home page
     </main>
   )
 }
