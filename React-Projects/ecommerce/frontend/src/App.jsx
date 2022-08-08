e350221149a9d9612d0366ed8433a8704f30af18
Debasish_R <rajaransingh@gmail.com>
2022-08-08 18:41:29 +0530

    feat:router and component added

---
+++ React-Projects/ecommerce/frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Routes from './Routes';
+import Navbar from './components/layout/header/Navbar'
+import Footer from './components/layout/footer/Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes/>
+    <Footer/>
+    <ToastContainer position="bottom-right" autoClose={2000} theme={'light'}/>
+    </>
+  );
+}
+
+export default App;
