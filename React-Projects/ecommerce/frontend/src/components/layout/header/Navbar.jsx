12c2fab829df04b0933ee1c0861e3d4c3f286e6f
Debasish_R <rajaransingh@gmail.com>
2022-08-09 14:50:00 +0530

    chore:navbar design done

--- React-Projects/ecommerce/frontend/src/components/layout/header/Navbar.jsx
+++ React-Projects/ecommerce/frontend/src/components/layout/header/Navbar.jsx
@@ -2,30 +2,53 @@ import './navbar.css'
 import '../../../utils/internetConnection'
 import { IoSearchOutline, IoCartOutline, IoSettingsOutline } from "react-icons/io5";
 import Theme from '../../../utils/Theme';
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const navMenu = [
+    {
+      link:"men",
+      label:"men"
+    },
+    {
+      link:"women",
+      label:"women"
+    },
+    {
+      link:"sports",
+      label:"sports"
+    },
+    {
+      link:"electronics",
+      label:"electronics"
+    },
+    {
+      link:"mobile",
+      label:"mobile"
+    },
+  ]
    
   return (
     <header className='container-fluid mainHeaderContainer'>
       <div className="container headerContainer">
-        <div className="row">
-          <div className="col-2 logo">
-            {true ? <img src="/images/logo/ecoshop-black.png" alt="ecoshop-logo" /> : <img src="/images/logo/ecoshop-white.png" alt="ecoshop-logo" />}
+          <div className="logo">
+            <Link to="/">{true ? <img src="/images/logo/ecoshop-black.png" alt="ecoshop-logo" /> : <img src="/images/logo/ecoshop-white.png" alt="ecoshop-logo" />}</Link>
           </div>
-          <div className="col-8 menus">
-            <p>Men</p>
-            <p>Women</p>
-            <p>Sports</p>
-            <p>Electronics</p>
-            <p>Mobiles</p>
+          <div className="menus">
+            {navMenu.map((menu)=>(
+              <NavLink to={`/${menu.link}`}>{menu.label}</NavLink>
+            ))}
+          </div>
+          <div className="userSettings">
+            <div className='userSetting_searchBox'>
+            <IoSearchOutline className='searchIcon'/>
+            <input type="text"/>
+            </div>
+            <Link to="/cart"><IoCartOutline/></Link>
+            <Link to="/setting"><IoSettingsOutline/></Link>
             <Theme/>
           </div>
-          <div className="col-2 userSettings">
-            <IoSearchOutline/>
-            <IoCartOutline/>
-            <IoSettingsOutline/>
-          </div>
-        </div>
       </div>
     </header>
   )
