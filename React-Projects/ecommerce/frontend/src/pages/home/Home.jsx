30c5cab1a5ac84f3371229de82fb84e40f20bcb4
Debasish_R <rajaransingh@gmail.com>
2022-08-23 02:03:58 +0530

    chore:carousel added to home page

--- React-Projects/ecommerce/frontend/src/pages/home/Home.jsx
+++ React-Projects/ecommerce/frontend/src/pages/home/Home.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './home.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Navigation, Pagination } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Home = () => {
   return (
     <main className='container-fluid mainHomeContainer'>
-        Home page
+        <div className='carousel'>
+        <Swiper
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay,Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide><img src="./images/products/pexels-laryssa-suaid-1667071.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-gabriel-freytez-341523.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-karolina-grabowska-5650016.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-math-90946.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-pixabay-256198.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-pixabay-279906.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-tree-of-life-seeds-3259600.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-karolina-grabowska-5650050.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="./images/products/pexels-tree-of-life-seeds-3259600.jpg" alt="" /></SwiperSlide>
+      </Swiper>
+        </div>
     </main>
   )
 }
