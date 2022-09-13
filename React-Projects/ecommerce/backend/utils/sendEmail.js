91affd48303734bccb063f6c6496ea951be5fecd
Debasish_R <rajaransingh@gmail.com>
2022-09-14 01:44:00 +0530

    fix: typo fix

--- React-Projects/ecommerce/backend/utils/sendEmail.js
+++ React-Projects/ecommerce/backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const sendEmail = async(options)=>{
     const transporter = nodeMailer.createTransport({
         host:process.env.SMTP_HOST,
         port:process.env.SMTP_PORT,
-        service:process.env.SMTP_SERVIVE,
+        service:process.env.SMTP_SERVICE,
         auth:{
             user:process.env.SMTP_MAIL,
             pass:process.env.SMTP_PASSWORD
