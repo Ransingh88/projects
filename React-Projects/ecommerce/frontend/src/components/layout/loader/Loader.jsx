e63a2bdea9cca19bee083923d1e994c97f733573
Debasish_R <rajaransingh@gmail.com>
2022-09-14 01:47:48 +0530

    feat: loader component created

---
+++ React-Projects/ecommerce/frontend/src/components/layout/loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './loader.css'
+
+const Loader = () => {
+  document.body.classList.add('no-scroll');
+  return (
+    <div className='loader'>
+      <div className='loaderWrapper'>
+      </div>
+        <img className='loaderImage' src="./images/logo/shopping-bagicon.png" alt="loader logo" />
+    </div>
+  )
+}
+
+export default Loader
