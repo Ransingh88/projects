75ebf584351dba7a3854059a12f4c0a8e90b7ed0
Debasish_R <rajaransingh@gmail.com>
2022-09-14 01:42:35 +0530

    chore: page per result change to 10

--- React-Projects/ecommerce/backend/controllers/productController.js
+++ React-Projects/ecommerce/backend/controllers/productController.js
@@ -15,7 +15,7 @@ exports.createProduct = catchAsyncErrors(async (req,res,next) => {
 
 // get all products
 exports.getAllProducts = catchAsyncErrors(async (req,res) =>{
-    const resultPerPage = 5
+    const resultPerPage = 10
     const productCount = await Product.countDocuments()
     const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage)
     const product = await apiFeature.query
