06d02487fae0cbe7e6cd2282980e8ddb29e603b8
Debasish_R <rajaransingh@gmail.com>
2022-09-14 01:48:12 +0530

    feat: product not found component created

---
+++ React-Projects/ecommerce/frontend/src/components/notFound/ProductNotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './productNotFound.css'
+
+const ProductNotFound = () => {
+  return (
+    <div className='productNotFound'><p>Product Not Found...</p></div >
+  )
+}
+
+export default ProductNotFound
