22964518ad1e690b66b9449a33e2ce2a25848de3
Debasish_R <rajaransingh@gmail.com>
2022-09-16 17:35:42 +0530

    feat: added product details route

--- React-Projects/ecommerce/frontend/src/routes/Routes.jsx
+++ React-Projects/ecommerce/frontend/src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from '../pages/home/Home'
+import ProductDetails from '../pages/productDetails/ProductDetails'
 
 const PageRoutes = () => {
   return (
     <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/product/:catagory/:id' element={<ProductDetails/>}/>
     </Routes>
   )
 }
