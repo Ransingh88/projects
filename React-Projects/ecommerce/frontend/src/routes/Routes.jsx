4b9630faca979aff2d5b8af908499df51a980f8d
Debasish_R <rajaransingh@gmail.com>
2022-10-05 17:53:02 +0530

    feat: new products route added

--- React-Projects/ecommerce/frontend/src/routes/Routes.jsx
+++ React-Projects/ecommerce/frontend/src/routes/Routes.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from '../pages/home/Home'
 import ProductDetails from '../pages/productDetails/ProductDetails'
+import Products from '../pages/products/Products'
 
 const PageRoutes = () => {
   return (
     <Routes>
         <Route exact path='/' element={<Home/>}/>
         <Route path='/product/:catagory/:id' element={<ProductDetails/>}/>
+        <Route exact path='/products' element={<Products/>}/>
+        <Route path='/products/:keyword' element={<Products/>}/>
         <Route path='*' element={<h1>404 not found</h1>}/>
     </Routes>
   )
