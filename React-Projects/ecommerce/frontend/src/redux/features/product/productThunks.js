54a26855c82d1b0022a6dca9ef3626c4610573b4
Debasish_R <rajaransingh@gmail.com>
2022-10-05 17:55:18 +0530

    chore: added filteration to all product get api

--- React-Projects/ecommerce/frontend/src/redux/features/product/productThunks.js
+++ React-Projects/ecommerce/frontend/src/redux/features/product/productThunks.js
@@ -1,14 +1,23 @@
 import axios from 'axios'
 import {getAllProductsFail, getAllProductsRequest, getAllProductsSuccess, clearErrors } from './productSlice'
 
-export const fetchAllProducts = () => async (dispatch)=>{
+export const fetchAllProducts = (keyword="",currentPage=1,price,catagory,ratings=0) => async (dispatch)=>{
     try {
         dispatch(getAllProductsRequest())
-        const res = await axios.get('/api/v1/products')
-        dispatch(getAllProductsSuccess(res.data))
+    
+        let url = `/api/v1/products?keyword=${keyword}&page=${currentPage}&ratings[gte]=${ratings}`
+        if(price){
+            url = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&ratings[gte]=${ratings}`
+        }
+        if(catagory){
+            url = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&catagory=${catagory}&rating[gte]=${ratings}`
+        }
+
+        const res = await axios.get(url)
+        dispatch(getAllProductsSuccess(res?.data))
         
     } catch (error) {
-        dispatch(getAllProductsFail(error.response.data.message))
+        dispatch(getAllProductsFail(error.response?.data.message))
     }
 }
 
