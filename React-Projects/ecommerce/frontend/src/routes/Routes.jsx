b23b411aab1801d393f633e44a89cc1b5d9072cf
Debasish_R <rajaransingh@gmail.com>
2022-10-08 19:28:41 +0530

    feat: auth route created

--- React-Projects/ecommerce/frontend/src/routes/Routes.jsx
+++ React-Projects/ecommerce/frontend/src/routes/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Auth from '../pages/auth/Auth'
 import Home from '../pages/home/Home'
 import ProductDetails from '../pages/productDetails/ProductDetails'
 import Products from '../pages/products/Products'
@@ -11,6 +12,7 @@ const PageRoutes = () => {
         <Route path='/product/:catagory/:id' element={<ProductDetails/>}/>
         <Route exact path='/products' element={<Products/>}/>
         <Route path='/products/:keyword' element={<Products/>}/>
+        <Route path='/auth/:authtype' element={<Auth/>}/>
         <Route path='*' element={<h1>404 not found</h1>}/>
     </Routes>
   )
