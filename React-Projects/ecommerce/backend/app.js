bac07fbf848b325587a951f3ad140e84501c73e9
Debasish_R <rajaransingh@gmail.com>
2022-10-11 20:25:22 +0530

    feat: cloudinary and file upload configured

--- React-Projects/ecommerce/backend/app.js
+++ React-Projects/ecommerce/backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
 const cors = require('cors')
 
 
@@ -9,6 +11,8 @@ const errorMiddleware = require('./middleware/error')
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(fileUpload())
 app.use(cors())
 
 
