70045c27be4796d3e4b1005ff34d5e3b3e6cb9d7
Debasish_R <rajaransingh@gmail.com>
2022-10-23 20:11:48 +0530

    feat: protected routes added

--- React-Projects/ecommerce/frontend/src/routes/Routes.jsx
+++ React-Projects/ecommerce/frontend/src/routes/Routes.jsx
@@ -4,8 +4,15 @@ import Auth from '../pages/auth/Auth'
 import Home from '../pages/home/Home'
 import ProductDetails from '../pages/productDetails/ProductDetails'
 import Products from '../pages/products/Products'
+import Profile from '../pages/profile/Profile'
+import {ROUTE_CONSTANTS} from '../constants/constants'
+// import { useSelector } from 'react-redux'
+import ProtectedRoute from '../components/protectedRoute/ProtectedRoute'
+import ProfileUpdate from '../components/profileUpdate/ProfileUpdate'
 
 const PageRoutes = () => {
+    // const {user,isAuthenticate,loading} = useSelector(state=>state.user)
+
   return (
     <Routes>
         <Route exact path='/' element={<Home/>}/>
@@ -13,6 +20,13 @@ const PageRoutes = () => {
         <Route exact path='/products' element={<Products/>}/>
         <Route path='/products/:keyword' element={<Products/>}/>
         <Route path='/auth/:authtype' element={<Auth/>}/>
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute/>}>
+          <Route exact path={`/${ROUTE_CONSTANTS.ACCOUNT}`} element={<Profile/>}>
+            <Route path='updateProfile' element={<ProfileUpdate/>}/>
+            <Route path='changePassword' element={<div>change password</div>}/>
+          </Route>
+        </Route>
         <Route path='*' element={<h1>404 not found</h1>}/>
     </Routes>
   )
