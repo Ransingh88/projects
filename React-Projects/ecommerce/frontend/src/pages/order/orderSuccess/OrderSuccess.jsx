b3c59a91cde44ad62aa38d32fb65c2e256572b8f
Debasish_R <rajaransingh@gmail.com>
2022-11-08 12:47:00 +0530

    feat: success order component created

---
+++ React-Projects/ecommerce/frontend/src/pages/order/orderSuccess/OrderSuccess.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './orderSuccess.css'
+import SucessIcon from '../../../Assets/image/icons/ok.gif'
+
+const OrderSuccess = () => {
+  return (
+    <main className='container-fluid mainOrderSuccessContainer'>
+        <section className='container orderSuccessContainer'>
+            <img src={SucessIcon} alt="" />
+            <p>Your order has been placed successfully</p>
+            <Link to='/order' className='btn viewOrderBtn'>view Order</Link>
+        </section>
+    </main>
+  )
+}
+
+export default OrderSuccess
