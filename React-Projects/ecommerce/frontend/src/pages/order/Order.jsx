999f3d9059de5b281a4ad9fc2d288f55213c3216
Debasish_R <rajaransingh@gmail.com>
2022-12-22 21:51:25 +0530

    fixed

--- React-Projects/ecommerce/frontend/src/pages/order/Order.jsx
+++ React-Projects/ecommerce/frontend/src/pages/order/Order.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './order.css'
 import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../../components/layout/loader/Loader'
